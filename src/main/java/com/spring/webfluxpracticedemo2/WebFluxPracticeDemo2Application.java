c46207532ba8e1a79f4081d4eb1e9d8c2840f2dc
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-09-13 16:24:51 +0530

    init commit

---
+++ src/main/java/com/spring/webfluxpracticedemo2/WebFluxPracticeDemo2Application.java
@@ -0,0 +1,13 @@
+package com.spring.webfluxpracticedemo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFluxPracticeDemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebFluxPracticeDemo2Application.class, args);
+	}
+
+}
