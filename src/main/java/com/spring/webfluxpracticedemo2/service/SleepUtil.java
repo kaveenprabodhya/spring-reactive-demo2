b1cd40c2a35455f87bb3dcfe905c88ee819738ac
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-09-13 19:20:27 +0530

    change to TimeUnit

--- src/main/java/com/spring/webfluxpracticedemo2/service/SleepUtil.java
+++ src/main/java/com/spring/webfluxpracticedemo2/service/SleepUtil.java
@@ -1,9 +1,11 @@
 package com.spring.webfluxpracticedemo2.service;
 
+import java.util.concurrent.TimeUnit;
+
 public class SleepUtil {
     public static void sleepSeconds(int seconds){
         try {
-            Thread.sleep(seconds);
+            TimeUnit.SECONDS.sleep(seconds);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
