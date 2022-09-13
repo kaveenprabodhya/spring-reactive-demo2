0f0d987c09fde3447bf51b1362d60c22d60ad9f5
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-09-13 23:04:55 +0530

    handle exception with pipeline approach

--- src/main/java/com/spring/webfluxpracticedemo2/controller/ReactiveMathValidationController.java
+++ src/main/java/com/spring/webfluxpracticedemo2/controller/ReactiveMathValidationController.java
@@ -4,6 +4,7 @@ import com.spring.webfluxpracticedemo2.dto.Response;
 import com.spring.webfluxpracticedemo2.exceptions.InputValidationException;
 import com.spring.webfluxpracticedemo2.service.ReactiveMathService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,5 +22,26 @@ public class ReactiveMathValidationController {
         if(input < 10 || input > 20)
             throw new InputValidationException(input);
         return this.mathService.findSquare(input);
+    }
+    @GetMapping("square/{input}/mono-error")
+    public Mono<Response> monoError(@PathVariable int input){
+        return Mono.just(input)
+                .handle((integer, responseSynchronousSink) -> {
+                    if(integer >= 10 && integer <= 20)
+                        responseSynchronousSink.next(integer);
+                    else
+                        responseSynchronousSink.error(new InputValidationException(integer));
+                })
+                .cast(Integer.class)
+                .flatMap(integer -> this.mathService.findSquare(integer));
+    }
+
+    @GetMapping("square/{input}/assignment")
+    public Mono<ResponseEntity<Response>> assignment(@PathVariable int input){
+        return Mono.just(input)
+                .filter(i -> i >= 10 && i <= 20)
+                .flatMap(i -> this.mathService.findSquare(i))
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.badRequest().build());
     }
 }
