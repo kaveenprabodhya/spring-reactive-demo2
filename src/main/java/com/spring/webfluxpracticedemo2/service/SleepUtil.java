44bf9f8f9dc6e47d389bf1f5fd96457dea072ab6
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-09-13 16:32:06 +0530

    surround with try catch and remove method signature exception

--- src/main/java/com/spring/webfluxpracticedemo2/service/SleepUtil.java
+++ src/main/java/com/spring/webfluxpracticedemo2/service/SleepUtil.java
@@ -1,7 +1,11 @@
 package com.spring.webfluxpracticedemo2.service;
 
 public class SleepUtil {
-    public static void sleepSeconds(int seconds) throws InterruptedException {
-        Thread.sleep(seconds);
+    public static void sleepSeconds(int seconds){
+        try {
+            Thread.sleep(seconds);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
