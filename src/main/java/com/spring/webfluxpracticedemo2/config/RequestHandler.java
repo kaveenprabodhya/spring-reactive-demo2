97c82dd97b82a9506d200025d20242ec8bbbdeaf
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-09-14 00:44:42 +0530

    add RequestHandler

---
+++ src/main/java/com/spring/webfluxpracticedemo2/config/RequestHandler.java
@@ -0,0 +1,49 @@
+package com.spring.webfluxpracticedemo2.config;
+
+import com.spring.webfluxpracticedemo2.dto.MultiplyRequestDTO;
+import com.spring.webfluxpracticedemo2.dto.Response;
+import com.spring.webfluxpracticedemo2.service.ReactiveMathService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class RequestHandler {
+
+    @Autowired
+    private ReactiveMathService mathService;
+
+    public Mono<ServerResponse> squareHandler(ServerRequest serverRequest){
+        int input = Integer.parseInt(serverRequest.pathVariable("input"));
+        Mono<Response> responseMono = this.mathService.findSquare(input);
+        return ServerResponse.ok().body(responseMono, Response.class);
+    }
+
+    public Mono<ServerResponse> tableHandler(ServerRequest serverRequest){
+        int input = Integer.parseInt(serverRequest.pathVariable("input"));
+        Flux<Response> responseFlux = this.mathService.multiplicationTable(input);
+        return ServerResponse.ok().body(responseFlux, Response.class);
+    }
+
+    public Mono<ServerResponse> tableStreamHandler(ServerRequest serverRequest){
+        int input = Integer.parseInt(serverRequest.pathVariable("input"));
+        Flux<Response> responseFlux = this.mathService.multiplicationTable(input);
+        return ServerResponse
+                .ok()
+                .contentType(MediaType.TEXT_EVENT_STREAM)
+                .body(responseFlux, Response.class);
+    }
+
+    public Mono<ServerResponse> multiplyHandler(ServerRequest serverRequest){
+        Mono<MultiplyRequestDTO> multiplyRequestDTOMono = serverRequest.bodyToMono(MultiplyRequestDTO.class);
+        Mono<Response> responseMono = this.mathService.multiply(multiplyRequestDTOMono);
+        return ServerResponse
+                .ok()
+                .contentType(MediaType.TEXT_EVENT_STREAM)
+                .body(responseMono, Response.class);
+    }
+}
