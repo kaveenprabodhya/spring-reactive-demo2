cd07cfc6cd0df40ea8b094a6ae3ef0295355a6ad
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-09-13 20:54:52 +0530

    impl multiply for two values

--- src/main/java/com/spring/webfluxpracticedemo2/service/ReactiveMathService.java
+++ src/main/java/com/spring/webfluxpracticedemo2/service/ReactiveMathService.java
@@ -1,5 +1,6 @@
 package com.spring.webfluxpracticedemo2.service;
 
+import com.spring.webfluxpracticedemo2.dto.MultiplyRequestDTO;
 import com.spring.webfluxpracticedemo2.dto.Response;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
@@ -18,5 +19,11 @@ public class ReactiveMathService {
                 .delayElements(Duration.ofSeconds(1))
                 .doOnNext(i -> System.out.println("reactive math service pricessing: "+i))
                 .map(i -> new Response(i * input));
+    }
+
+    public Mono<Response> multiply(Mono<MultiplyRequestDTO> dtoMono){
+        return dtoMono
+                .map(multiplyRequestDTO -> multiplyRequestDTO.getFirst() * multiplyRequestDTO.getSecond())
+                .map(Response::new);
     }
 }
