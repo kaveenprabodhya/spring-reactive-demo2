afc49943d8c7ee1c51f9703564236531c91933c1
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-09-13 19:20:42 +0530

    add reactive math service

---
+++ src/main/java/com/spring/webfluxpracticedemo2/service/ReactiveMathService.java
@@ -0,0 +1,22 @@
+package com.spring.webfluxpracticedemo2.service;
+
+import com.spring.webfluxpracticedemo2.dto.Response;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
+@Service
+public class ReactiveMathService {
+    public Mono<Response> findSquare(int input){
+        return Mono.fromSupplier(() -> input*input).map(Response::new);
+    }
+
+    public Flux<Response> multiplicationTable(int input){
+        return Flux.range(1,10)
+                .delayElements(Duration.ofSeconds(1))
+                .doOnNext(i -> System.out.println("reactive math service pricessing: "+i))
+                .map(i -> new Response(i * input));
+    }
+}
