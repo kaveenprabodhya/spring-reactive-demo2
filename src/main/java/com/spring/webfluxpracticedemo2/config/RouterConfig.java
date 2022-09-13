510990415df54bc2ef9e16a987507af120629de3
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-09-14 00:44:55 +0530

    add RouterConfig

---
+++ src/main/java/com/spring/webfluxpracticedemo2/config/RouterConfig.java
@@ -0,0 +1,25 @@
+package com.spring.webfluxpracticedemo2.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+@Configuration
+public class RouterConfig {
+
+    @Autowired
+    private RequestHandler requestHandler;
+
+    @Bean
+    public RouterFunction<ServerResponse> serverResponseRouterFunction(){
+        return RouterFunctions.route()
+                .GET("router/square/{input}", requestHandler::squareHandler)
+                .GET("router/table/{input}", requestHandler::tableHandler)
+                .GET("router/table/{input}/stream", requestHandler::tableStreamHandler)
+                .POST("router/multiply", requestHandler::multiplyHandler)
+                .build();
+    }
+}
