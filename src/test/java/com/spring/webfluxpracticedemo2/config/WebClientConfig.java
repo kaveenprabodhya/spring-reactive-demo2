bcf24cde458955396aeec9671a78778977e4d9c8
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-09-14 18:15:46 +0530

    session token setting

--- src/test/java/com/spring/webfluxpracticedemo2/config/WebClientConfig.java
+++ src/test/java/com/spring/webfluxpracticedemo2/config/WebClientConfig.java
@@ -2,7 +2,11 @@ package com.spring.webfluxpracticedemo2.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.ClientRequest;
+import org.springframework.web.reactive.function.client.ClientResponse;
+import org.springframework.web.reactive.function.client.ExchangeFunction;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Configuration
 public class WebClientConfig {
@@ -11,6 +15,15 @@ public class WebClientConfig {
         return WebClient.builder()
                 .baseUrl("http://localhost:8080")
 //                .defaultHeaders(h -> h.setBasicAuth("username", "password"))
+                .filter(this::sessionToken)
                 .build();
+    }
+
+    private Mono<ClientResponse> sessionToken(ClientRequest request, ExchangeFunction function){
+        System.out.println("Generating session token");
+        ClientRequest clientRequest = ClientRequest
+                .from(request)
+                .headers(httpHeaders -> httpHeaders.setBearerAuth("some-lengthy-jwt")).build();
+        return function.exchange(clientRequest);
     }
 }
