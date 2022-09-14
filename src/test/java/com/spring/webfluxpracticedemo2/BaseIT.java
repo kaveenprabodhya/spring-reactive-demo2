49aebd62ea40319bc92d504e840fbf038c6245b0
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-09-14 14:40:07 +0530

    add BaseIT

---
+++ src/test/java/com/spring/webfluxpracticedemo2/BaseIT.java
@@ -0,0 +1,8 @@
+package com.spring.webfluxpracticedemo2;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+class BaseIT {
+
+}
