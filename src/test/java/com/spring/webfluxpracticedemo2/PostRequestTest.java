9694afdff343dbd12c20c58dff3b9099f8405c6f
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-09-14 15:52:33 +0530

    test multiply on postTest

--- src/test/java/com/spring/webfluxpracticedemo2/PostRequestTest.java
+++ src/test/java/com/spring/webfluxpracticedemo2/PostRequestTest.java
@@ -1,4 +1,36 @@
 package com.spring.webfluxpracticedemo2;
 
+import com.spring.webfluxpracticedemo2.dto.MultiplyRequestDTO;
+import com.spring.webfluxpracticedemo2.dto.Response;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
 public class PostRequestTest extends BaseIT {
+    @Autowired
+    private WebClient webClient;
+
+    @Test
+    public void postTest(){
+        Mono<Response> responseMono = this.webClient
+                .post()
+                .uri("reactive-math/multiply")
+                .bodyValue(buildRequestDto(5, 2))
+                .retrieve()
+                .bodyToMono(Response.class)
+                .doOnNext(System.out::println);
+
+        StepVerifier.create(responseMono)
+                .expectNextCount(1)
+                .verifyComplete();
+    }
+
+    private MultiplyRequestDTO buildRequestDto(int a, int b){
+        MultiplyRequestDTO requestDTO = new MultiplyRequestDTO();
+        requestDTO.setFirst(a);
+        requestDTO.setSecond(b);
+        return requestDTO;
+    }
 }
