e7acbece9b6541067259c68b079363fe44aff286
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-09-14 17:05:48 +0530

    add protected WebClient prop

--- src/test/java/com/spring/webfluxpracticedemo2/BaseIT.java
+++ src/test/java/com/spring/webfluxpracticedemo2/BaseIT.java
@@ -1,8 +1,11 @@
 package com.spring.webfluxpracticedemo2;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class BaseIT {
-
+    @Autowired
+    protected WebClient webClient;
 }
