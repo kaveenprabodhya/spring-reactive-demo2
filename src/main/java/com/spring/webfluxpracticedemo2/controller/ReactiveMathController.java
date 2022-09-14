528ae60ef7d252648b9057b96c6882832c0f7e97
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-09-14 16:19:13 +0530

    get requestHeader of multiply

--- src/main/java/com/spring/webfluxpracticedemo2/controller/ReactiveMathController.java
+++ src/main/java/com/spring/webfluxpracticedemo2/controller/ReactiveMathController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.Map;
+
 
 @RestController
 @RequestMapping("reactive-math")
@@ -31,7 +33,8 @@ public class ReactiveMathController {
     }
 
     @PostMapping("multiply")
-    public Mono<Response> multiply(@RequestBody Mono<MultiplyRequestDTO> dtoMono){
+    public Mono<Response> multiply(@RequestBody Mono<MultiplyRequestDTO> dtoMono, @RequestHeader Map<String, String> headers){
+        System.out.println(headers);
         return this.mathService.multiply(dtoMono);
     }
 }
